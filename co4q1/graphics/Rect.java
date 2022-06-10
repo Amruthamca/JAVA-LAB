a29fcd251e4e23b0eff74981cd57de72d5c9cb1b
Amruthamca <94528537+Amruthamca@users.noreply.github.com>
2022-06-10 15:15:16 +0530

    Add files via upload

---
+++ co4q1/graphics/Rect.java
@@ -0,0 +1,19 @@
+package graphics;
+
+/**
+ *
+ * @author sjcet
+ */
+import java.util.Scanner;
+public class Rect implements area_cal{
+     int l,b;
+    
+    public void area(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle :");
+        l = sc.nextInt();
+        System.out.println("Enter the breath of the rectangle");
+        b = sc.nextInt();
+        System.out.println("Area of the rectangle = "+l*b);
+    }
+}
